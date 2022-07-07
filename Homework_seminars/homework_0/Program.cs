0fa79ca3fea73f7567b6157449b8aa2d6c89f3ca
mikhail <mikhail.vakulin@gmail.com>
2022-07-07 21:57:51 +0300

    написал код для задачи с собакой

---
+++ Homework_seminars/homework_0/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int count = 0, distance = 10000, FirstFriendSpeed = 1, SecondFriendSpeed = 2, DogSpeed = 5, Friend = 2, time;
+
+
+        while (distance > 10)
+        {
+            if (Friend == 1)
+            {
+                time = distance / (FirstFriendSpeed + DogSpeed);
+                Friend = 2;
+            }
+            else
+            {
+                time = distance / (SecondFriendSpeed + DogSpeed);
+                Friend = 1;
+            }
+            distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+}
