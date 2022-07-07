b7e843e61f4f8add32f4f5cef4b6bd42d497e81e
mikhail <mikhail.vakulin@gmail.com>
2022-07-07 11:05:24 +0300

    next step

---
+++ Examples/Examples005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "миша")
+{
+    Console.WriteLine("Да это же наш повелитель!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
